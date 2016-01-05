c86fc92dce35ad0b8bbb96cb4f4285ba294134c1
maodahua <289203021@qq.com>
2016-01-05 13:04:10 +0800

    增加注释

--- src/com/maodahua/aplmap/BasicMapActivity.java
+++ src/com/maodahua/aplmap/BasicMapActivity.java
@@ -1,39 +1,51 @@
+//created by maodahua
+//2015.12.10
+//copyright maodahua
+
 package com.maodahua.aplmap;
 
 import com.amap.api.maps2d.AMap;
 import com.amap.api.maps2d.CameraUpdate;
 import com.amap.api.maps2d.CameraUpdateFactory;
 import com.amap.api.maps2d.MapView;
-import com.amap.api.maps2d.model.CameraPosition;
 import com.amap.api.maps2d.model.LatLng;
 import com.amap.api.maps2d.model.MarkerOptions;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
 
+/**
+ * this Activity is show a basic mapView
+ * @author MDH
+ *
+ */
 public class BasicMapActivity extends Activity {
 	
 	private MapView mapView;
 	private AMap aMap;
+	//use tow variables to receive values from first Activity
 	private double la,lo;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.basicmap_activity);
-		mapView = (MapView) findViewById(R.id.map);
-		mapView.onCreate(savedInstanceState);// 此方法必须重写
+		
+		mapView = (MapView)findViewById(R.id.map);
+		//you must override this method
+		mapView.onCreate(savedInstanceState);
 		init();
+		
+		//use Intent to transfer values
 		Intent it = getIntent();
 		la = it.getDoubleExtra("la", 30);
 		lo = it.getDoubleExtra("lo", 30);
 		//Toast.makeText(this, la+"  "+lo+"", Toast.LENGTH_LONG).show();
-
+		
+		//set camera on map by latlng and zoom(3-20)
 		CameraUpdate cu = CameraUpdateFactory.newLatLngZoom(new LatLng(la, lo),15);
 		aMap.moveCamera(cu);
+		//set a mark on map
 		MarkerOptions mo = new MarkerOptions();
 		aMap.addMarker(mo.position(new LatLng(la, lo)));
 		
@@ -43,7 +55,6 @@ public class BasicMapActivity extends Activity {
 		if (aMap == null) {
 			aMap = mapView.getMap();
 		}
-		
 	}
 	
 	@Override
--- src/com/maodahua/aplmap/MainActivity.java
+++ src/com/maodahua/aplmap/MainActivity.java
@@ -1,6 +1,9 @@
+//created by maodahua
+//2015.12.10
+//copyright maodahua
+
 package com.maodahua.aplmap;
 
-import android.R.string;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -9,10 +12,13 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+
 public class MainActivity extends Activity {
+	
 	TextView t1, t2;
 	Location mLocation;
 	LocationManager mLocationManager;
@@ -23,35 +29,39 @@ public class MainActivity extends Activity {
 		
 		t1 = (TextView)findViewById(R.id.textView1);
 		t2 = (TextView)findViewById(R.id.textView2);
-		
 		mLocation = getLocation();
-		
 	}
 	
+	/**
+	 * return a location which get values by this method.
+	 * @return
+	 */
 	private Location getLocation()
 	{
 		mLocationManager = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
-		
 		Criteria cr = new Criteria();
+		//set max accuracy
 		cr.setAccuracy(Criteria.ACCURACY_FINE);
+		//set altitude
 		cr.setAltitudeRequired(false);
+		//set bearing
 		cr.setBearingRequired(false);
+		//set cost
 		cr.setCostAllowed(true);
+		//set power
 		cr.setPowerRequirement(Criteria.POWER_MEDIUM);
 		
+		//find a best provider
 		String provider = mLocationManager.getBestProvider(cr, true);
-		
+		//get the last location
 		Location location = mLocationManager.getLastKnownLocation(provider);
-		
-		mLocationManager.requestLocationUpdates(provider, 2000, 5, locationListener);
+		//2sec 100metres 
+		mLocationManager.requestLocationUpdates(provider, 2000, 100, locationListener);
 		return location;
 	}
 	
 	private final LocationListener locationListener = new LocationListener()
 	{
-		
-	
-		
 		@Override
 		public void onStatusChanged(String provider, int status, Bundle extras) {
 			// TODO Auto-generated method stub
@@ -72,10 +82,10 @@ public class MainActivity extends Activity {
 		
 		@Override
 		public void onLocationChanged(Location location) {
-			// TODO Auto-generated method stub
 			if(location != null)
 			{
 				t1.setText("纬度"+location.getLatitude());
+				Log.d("location", location.getLatitude()+"");
 				t2.setText("经度"+location.getLongitude());
 			}
 		}
